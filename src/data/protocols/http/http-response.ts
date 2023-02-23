c6a6f7494a99ade669fa24a8a7ed8d7c1ee6356b
Gustavo Gava <gustavogava123@hotmail.com>
2023-02-23 07:41:41 -0300

    feat: ensure RemoteAuthentication throws InvalidCredentialsError when StatusCode is 401

---
+++ src/data/protocols/http/http-response.ts
@@ -0,0 +1,11 @@
+export enum HttpStatusCode {
+  ok = 200,
+  noContent = 204,
+  badRequest = 400,
+  unauthorized = 401,
+}
+
+export type HttpResponse = {
+  statusCode: HttpStatusCode
+  body?: any
+}
