603049f8abb743c40636f9d4436e3552e21bb2e0
Gustavo Gava <gustavogava123@hotmail.com>
2023-02-28 08:30:49 -0300

    test: ensure AxiosHttpClient calls axios with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,16 @@
+import { randUrl } from '@ngneat/falso'
+import axios from 'axios'
+import { AxiosHttpClient } from './axios-http-client'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct URL', async () => {
+    const url = randUrl()
+    const sut = new AxiosHttpClient()
+
+    await sut.post({ url })
+    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+  })
+})
