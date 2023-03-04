73994c66165c9e9603b418024cad9171030f3bd1
Gustavo Gava <gustavogava123@hotmail.com>
2023-03-04 09:13:38 -0300

    test: ensure AxiosHttpClient calls axios with correct params

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,20 +1,44 @@
-import { randUrl } from '@ngneat/falso'
+import { type HttpPostParams } from '@/data/protocols/http'
+import { randEmail, randPassword, randUrl, randUserName } from '@ngneat/falso'
 import axios from 'axios'
 import { AxiosHttpClient } from './axios-http-client'
 
 jest.mock('axios')
-const mockedAxios = axios as jest.Mocked<typeof axios>
 
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
+interface SutTypes {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
 }
 
-describe('AxiosHttpClient', () => {
-  test('Should call axios with correct URL and verb', async () => {
-    const url = randUrl()
-    const sut = makeSut()
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = axios as jest.Mocked<typeof axios>
 
-    await sut.post({ url })
-    expect(mockedAxios.post).toHaveBeenCalledWith(url)
+  return {
+    sut,
+    mockedAxios,
+  }
+}
+
+const mockPostRequest = (): HttpPostParams<any> => {
+  return {
+    url: randUrl(),
+    body: {
+      name: randUserName(),
+      email: randEmail(),
+      password: randPassword(),
+    },
+  }
+}
+
+// TODO - Join the second test with the first one
+
+describe('AxiosHttpClient', () => {
+  test('Should call axios with correct params', async () => {
+    const { url, body } = mockPostRequest()
+    const { sut, mockedAxios } = makeSut()
+
+    await sut.post({ url, body })
+    expect(mockedAxios.post).toHaveBeenCalledWith(url, body)
   })
 })
