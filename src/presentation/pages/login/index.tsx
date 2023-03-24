99ac44d109e78045c06a9338b6b7016d6f3def12
Gustavo Gava <gustavogava123@hotmail.com>
2023-03-24 16:18:34 -0300

    feat: added formStatus to Login component

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,6 +1,19 @@
 import { Button, Footer, Input, LoginHeader } from '@/presentation/components'
+import { useState } from 'react'
+
+type FormStateProps = {
+  isLoading: boolean
+  emailErrorMessage: string
+  passwordErrorMessage: string
+}
 
 const Login = () => {
+  const [formState] = useState<FormStateProps>({
+    isLoading: false,
+    emailErrorMessage: '',
+    passwordErrorMessage: '',
+  })
+
   return (
     <div className='flex h-screen w-full flex-col justify-between bg-slate-100'>
       <LoginHeader />
@@ -8,11 +21,21 @@ const Login = () => {
       <form className='mx-auto flex w-full flex-col items-center justify-center gap-4 rounded bg-white p-4 text-xl shadow-md md:w-96 md:p-8'>
         <h2 className='font-bold'>Login</h2>
 
-        <Input type='email' placeholder='Digite seu e-mail' />
+        <Input
+          type='email'
+          placeholder='Digite seu e-mail'
+          status='untouched'
+        />
 
-        <Input type='password' placeholder='Digite sua senha' />
+        <Input
+          type='password'
+          placeholder='Digite sua senha'
+          status='success'
+        />
 
-        <Button type='submit'>Entrar</Button>
+        <Button type='submit' isLoading={formState.isLoading}>
+          Entrar
+        </Button>
 
         <span className='mt-2 text-base text-primary hover:cursor-pointer hover:underline'>
           não possui conta? cadastre-se
