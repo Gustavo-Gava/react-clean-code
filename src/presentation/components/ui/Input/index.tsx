c9cde9d992dcf79c71ec87af7c5b4a98d91e3e2b
Gustavo Gava <gustavogava123@hotmail.com>
2023-03-24 16:15:59 -0300

    refactor: add status property to Input component

--- src/presentation/components/ui/Input/index.tsx
+++ src/presentation/components/ui/Input/index.tsx
@@ -5,33 +5,53 @@ import { FiCheckCircle, FiXCircle } from 'react-icons/fi'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   containerProps?: React.HTMLAttributes<HTMLDivElement>
+  status?: 'error' | 'success' | 'untouched'
 }
 
-const isValid = true
-const error = false
-
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ containerProps, className, ...rest }, ref) => {
-    const border = isValid ? 'border-green-500' : 'border-primary-light'
+  ({ containerProps, className, status = 'untouched', type, ...rest }, ref) => {
+    const borderColor =
+      status === 'success'
+        ? 'border-green-500'
+        : status === 'error'
+        ? 'border-primary-light'
+        : 'border-gray-300'
 
     return (
       <div
         className={twMerge(
-          `group flex w-full items-center justify-between rounded border-[1px] p-2 outline-double outline-0 outline-primary [&:has(input:focus)]:outline-1 ${border}`,
+          `group flex w-full items-center justify-between rounded border-[1px] p-2 outline-double outline-0 outline-primary [&:has(input:focus)]:outline-1 ${borderColor}`,
           className
         )}
+        role='input-wrapper'
         {...containerProps}
       >
         <input
-          type='password'
           className='flex flex-1 rounded text-base outline-none placeholder:text-sm'
+          aria-label={type}
+          type={type}
           ref={ref}
           {...rest}
         />
 
-        {isValid && <FiCheckCircle className='text-green-500' size={22} />}
+        {status === 'success' && (
+          <FiCheckCircle
+            role='status'
+            className='text-green-500'
+            size={22}
+            aria-label='success-icon'
+          />
+        )}
 
-        {error && <FiXCircle className='text-red-500' size={22} />}
+        {status === 'error' && (
+          <FiXCircle
+            role='status'
+            title='error-icon'
+            aria-label='error-icon'
+            className='text-red-500'
+            size={22}
+          />
+        )}
       </div>
     )
   }
