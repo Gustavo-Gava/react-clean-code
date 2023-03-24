60471c7a28354d946256020f97c4aa3ad1a5931e
Gustavo Gava <gustavogava123@hotmail.com>
2023-03-24 16:17:03 -0300

    feat: disable button on loading

--- src/presentation/components/ui/Button/index.tsx
+++ src/presentation/components/ui/Button/index.tsx
@@ -9,7 +9,8 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ children, isLoading = false, ...rest }: ButtonProps) {
   return (
     <button
-      className='flex h-12 w-full items-center justify-center rounded-md bg-primary p-2 text-base text-white duration-200 hover:bg-primary-light'
+      className='flex h-12 w-full items-center justify-center rounded-md bg-primary p-2 text-base text-white duration-200 hover:bg-primary-light disabled:cursor-not-allowed disabled:bg-gray-300'
+      disabled={isLoading || rest.disabled}
       {...rest}
     >
       {isLoading ? <LoadingRing /> : children}
