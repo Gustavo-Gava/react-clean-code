6029a8e972f9fecbdc7b5794c8e9c36a24eb1c79
Gustavo Gava <gustavogava123@hotmail.com>
2023-03-28 08:54:00 -0300

    feat: ensure login is calling validation method

--- src/presentation/components/ui/Input/index.tsx
+++ src/presentation/components/ui/Input/index.tsx
@@ -5,11 +5,22 @@ import { FiCheckCircle, FiXCircle } from 'react-icons/fi'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   containerProps?: React.HTMLAttributes<HTMLDivElement>
+  errorMessage?: string
   status?: 'error' | 'success' | 'untouched'
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ containerProps, className, status = 'untouched', type, ...rest }, ref) => {
+  (
+    {
+      containerProps,
+      className,
+      status = 'untouched',
+      errorMessage,
+      type,
+      ...rest
+    },
+    ref
+  ) => {
     const borderColor =
       status === 'success'
         ? 'border-green-500'
@@ -18,39 +29,52 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         : 'border-gray-300'
 
     return (
-      <div
-        className={twMerge(
-          `group flex w-full items-center justify-between rounded border-[1px] p-2 outline-double outline-0 outline-primary [&:has(input:focus)]:outline-1 ${borderColor}`,
-          className
-        )}
-        role='input-wrapper'
-        {...containerProps}
-      >
-        <input
-          className='flex flex-1 rounded text-base outline-none placeholder:text-sm'
-          aria-label={type}
-          type={type}
-          ref={ref}
-          {...rest}
-        />
-
-        {status === 'success' && (
-          <FiCheckCircle
-            role='status'
-            className='text-green-500'
-            size={22}
-            aria-label='success-icon'
+      <div className='flex w-full flex-col'>
+        <div
+          className={twMerge(
+            `group flex w-full items-center justify-between rounded border-[1px] p-2 outline-double outline-0 outline-primary [&:has(input:focus)]:outline-1 ${borderColor}`,
+            className
+          )}
+          role='input-wrapper'
+          {...containerProps}
+        >
+          <input
+            className='flex flex-1 rounded text-base outline-none placeholder:text-sm'
+            title={type}
+            aria-label={type}
+            type={type}
+            ref={ref}
+            {...rest}
           />
-        )}
+
+          {status === 'success' && (
+            <FiCheckCircle
+              role='status'
+              className='text-green-500'
+              size={22}
+              aria-label='success-icon'
+            />
+          )}
+
+          {status === 'error' && (
+            <FiXCircle
+              role='status'
+              title='error-icon'
+              aria-label='error-icon'
+              className='text-red-500'
+              size={22}
+            />
+          )}
+        </div>
 
         {status === 'error' && (
-          <FiXCircle
-            role='status'
-            title='error-icon'
-            aria-label='error-icon'
-            className='text-red-500'
-            size={22}
-          />
+          <span
+            className='mt-1 text-xs text-red-600'
+            role='error-message'
+            aria-label='error-message'
+          >
+            {errorMessage}
+          </span>
         )}
       </div>
     )
--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,18 +1,62 @@
 import { Button, Footer, Input, LoginHeader } from '@/presentation/components'
+import { type Validation } from '@/presentation/protocols/validation'
 import { useState } from 'react'
+
+type InputStatus = 'untouched' | 'success' | 'error'
+
+type InputStateProps = {
+  status: 'untouched' | 'success' | 'error'
+  errorMessage: string
+  value: string
+}
 
 type FormStateProps = {
   isLoading: boolean
-  emailErrorMessage: string
-  passwordErrorMessage: string
+  email: InputStateProps
+  password: InputStateProps
 }
 
-const Login = () => {
-  const [formState] = useState<FormStateProps>({
+type Props = {
+  validation?: Validation
+}
+
+const Login = ({ validation }: Props) => {
+  const [formState, setFormState] = useState<FormStateProps>({
     isLoading: false,
-    emailErrorMessage: '',
-    passwordErrorMessage: '',
+    email: {
+      status: 'error' as InputStatus,
+      errorMessage: 'Campo Obrigatório',
+      value: '',
+    },
+    password: {
+      status: 'error' as InputStatus,
+      errorMessage: 'Campo Obrigatório',
+      value: '',
+    },
   })
+
+  const onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target
+    const errorMessage = validation?.validate({ [name]: value })
+    const hasError = !(errorMessage !== null)
+
+    // setFormState({
+    //   ...formState,
+    //   [name]: {
+    //     status: 'success',
+    //     errorMessage: '',
+    //     value,
+    //   },
+    // })
+
+    setFormState({
+      ...formState,
+      [name]: {
+        status: hasError ? 'error' : 'success',
+        errorMessage: errorMessage ?? '',
+      },
+    })
+  }
 
   return (
     <div className='flex h-screen w-full flex-col justify-between bg-slate-100'>
@@ -23,17 +67,32 @@ const Login = () => {
 
         <Input
           type='email'
+          name='email'
           placeholder='Digite seu e-mail'
-          status='untouched'
+          errorMessage={formState.email.errorMessage}
+          status={formState.email.status}
+          onChange={onInputChange}
+          value={formState.email.value}
         />
 
         <Input
           type='password'
+          name='password'
           placeholder='Digite sua senha'
-          status='success'
+          errorMessage={formState.password.errorMessage}
+          status={formState.password.status}
+          onChange={onInputChange}
+          value={formState.password.value}
         />
 
-        <Button type='submit' isLoading={formState.isLoading}>
+        <Button
+          type='submit'
+          isLoading={formState.isLoading}
+          disabled={
+            formState.email.status === 'error' ||
+            formState.password.status === 'error'
+          }
+        >
           Entrar
         </Button>
 
